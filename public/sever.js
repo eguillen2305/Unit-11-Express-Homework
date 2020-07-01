485745f0d83249f74f7f89f189efa1fc824e30c2
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-01 16:13:35 -0500

    Added constants required

--- public/sever.js
+++ public/sever.js
@@ -1,8 +1,7 @@
 //START OF SERVER.JS
-const express = require("express");
-
-
-
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const app = express();
+const PORT = process.env.PORT || 3036;
 
 app.use(express.json());
-
