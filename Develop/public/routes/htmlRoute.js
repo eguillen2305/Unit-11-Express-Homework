707ab269f2d04a57a9622c52ae71afb8e4df0cf5
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-02 18:08:30 -0500

    fixed ENOENT error for HTML but now getting same error for notes.html

--- Develop/public/routes/htmlRoute.js
+++ Develop/public/routes/htmlRoute.js
@@ -4,9 +4,9 @@ const path = require('path');
 module.exports = function(app) {
 	// app.get to get notes on notes.html
 	app.get('/notes/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../public/notes.html'));
+		res.sendFile(path.join(__dirname, '../notes.html'));
 	});
 	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../public/index.html'));
+		res.sendFile(path.join(__dirname, '../index.html'));
 	});
 };
