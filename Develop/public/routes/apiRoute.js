b8f26db4f79f14d9281749ac20d3b138a41a1c7e
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-02 12:15:06 -0500

    updated apiRoute.js
    added app.get, app.delete, and app.post

---
+++ Develop/public/routes/apiRoute.js
@@ -0,0 +1,60 @@
+//Dependencies
+const fs = require('fs');
+const data = require('../db/db.json');
+
+// Routing
+module.exports = (app) => {
+	// API GET routes
+	app.get('/api/notes', (request, result) => {
+		fs.readFile('./db/db.json', 'utf-8', (err, notes) => {
+			if (err) throw err;
+			let notesParsed;
+			try {
+				notesParsed = [].concat(JSON.parse(notes));
+			} catch (err) {
+				notesParsed = [];
+			}
+			result.json(notesParsed);
+		});
+	});
+
+	app.delete('/api/notes/:id', (request, result) => {
+		let noteId = request.params.id;
+		console.log('noteId: ', noteId);
+
+		fs.readFile('./db/db.json', 'utf-8', (err, response) => {
+			if (err) throw err;
+			const allNotes = JSON.parse(response);
+			const newNotes = allNotes.filter((note) => note.id != noteId);
+			fs.writeFile('./db/db.json', JSON.stringify(newNotes, null, 2), (err) => {
+				if (err) throw err;
+				result.json(true);
+				console.log('Notes deleted!');
+			});
+		});
+	});
+
+	//APP.POST api notes
+	app.post('./api/notes', (request, result) => {
+		console.log(data, 'Adding New Note', request.body);
+		// Read from the JSON file
+		fs.readFile('./db/db.json', 'utf-8', (err, response) => {
+			// Convert to JSON
+			let allNotes = JSON.parse(response);
+
+			var latestNote = allNotes[allNotes.length - 1].id;
+			latestNote = latestNote + 1;
+			console.log(latestNote);
+
+			const newNote = { ...req.body, id: latestNote };
+			console.log('New Note: ', newNote);
+			allNotes.push(newNote);
+			// Updates the new note added
+			fs.writeFile('./db/db.json', JSON.stringify(allNotes), (err) => {
+				if (err) throw err;
+				result.json(allNotes);
+				console.log('New note has been added: ', allNotes);
+			});
+		});
+	});
+};
