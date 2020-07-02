ba1357aefcc80c1bc22f7acf4c25efc9f707dfe9
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-02 11:24:45 -0500

    Updated HTML Route
    module exports, app.get, send file should complete this file.

--- Develop/public/routes/htmlRoute.js
+++ Develop/public/routes/htmlRoute.js
@@ -1,7 +1,12 @@
-const path = require("path");
+const path = require('path');
 
-module.exports = app function{
-    app.get("/notes/", (req, res) function{
-        res.sendFile (path.join)
-    })
-}
+// Routing
+module.exports = (app) => {
+	// app.get to get notes on notes.html
+	app.get('/notes/', (req, res) => {
+		res.sendFile(path.join(__dirname, '../public/notes.html'));
+	});
+	app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname, '../public/index.html'));
+	});
+};
