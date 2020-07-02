c05f6784c077726ffbc86da664f284250fdff034
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-02 12:47:58 -0500

    added app.use express statments
    app.use express urlencoded and static.

--- Develop/public/sever.js
+++ Develop/public/sever.js
@@ -2,10 +2,20 @@
 const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+
+
 
 app.listen(PORT, function() {
 	console.log(`SERVER IS RUNNING ON PORT: ${PORT}`);
 });
+
+
+
