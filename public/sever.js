b61fbfba7cf1ba4a79c3461b937d180a37ca9a2e
Eduardo Guillen <eguillen2035@gmail.com>
2020-07-02 10:25:39 -0500

    Added app.listen PORT console.log

--- public/sever.js
+++ public/sever.js
@@ -5,3 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3036;
 
 app.use(express.json());
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log(`SERVER IS RUNNING ON PORT: ${PORT}`);
+})
